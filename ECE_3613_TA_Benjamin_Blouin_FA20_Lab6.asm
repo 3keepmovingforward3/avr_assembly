4b966abdcac4bfd041207ad53df19617a143c4ec
Benjamin Blouin <3keepmovingforward3@users.noreply.github.com>
2020-03-09 08:16:48 -0400

    Create ECE_3613_TA_Benjamin_Blouin_FA20_Lab6.asm
    initial commit

---
+++ ECE_3613_TA_Benjamin_Blouin_FA20_Lab6.asm
@@ -0,0 +1,12 @@
+;
+; ECE_3613_TA_Benjamin_Blouin_FA20_Lab6.asm
+;
+; Created: 3/9/2020 8:16:16 AM
+; Author : Benjamin Blouin
+;
+
+
+; Replace with your application code
+main:
+    inc r16
+    rjmp main
